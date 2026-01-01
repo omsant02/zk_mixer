6ecae6a27cb06f0ba641b03d716a913c28e994d7
om santoshwar <omsantoshwar02@gmail.com>
2026-01-01 22:43:03 +0530

    implement IMT and deposits

--- contracts/src/Mixer.sol
+++ contracts/src/Mixer.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract Mixer {
+import {IVerifier} from "./IVerifier.sol";
+import {IncrementalMerkleTree, Poseidon2} from "./IncrementalMerkleTree.sol";
+
+contract Mixer is IncrementalMerkleTree{
     IVerifier public immutable i_verifier;
 
     mapping (bytes32 => bool) public s_commitments;
@@ -12,7 +15,7 @@ contract Mixer {
     error Mixer__CommitmentAlreadyAdded(bytes32 commitment);
     error Mixer__DepositAmountNotCorrect(uint256 amountSent, uint256 expectedAmount);
 
-    constructor(IVerifier _verifier) {
+    constructor(IVerifier _verifier, uint32 _merkleTreeDepth, Poseidon2 _hasher) IncrementalMerkleTree(_merkleTreeDepth, _hasher){
         i_verifier = _verifier;
     }
 
@@ -23,7 +26,10 @@ contract Mixer {
         if(msg.value != DENOMINATION) {
             revert Mixer__DepositAmountNotCorrect(msg.value, DENOMINATION);
         }
+        uint32 insertedIndex = _insert(_commitment);
         s_commitments[_commitment] = true;
+
+        emit Deposit(_commitment, insertedIndex, block.timestamp);
     }
 
     function withdraw(bytes calldata _proof) external {
